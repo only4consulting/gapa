5481870cf821bc471709eadcf472f61506922bed
only4consulting <only4consulting@gmail.com>
2019-01-08 09:48:58 -0300

    ENVAR FIX1

--- src/pages/QuieroColaborar/index.js
+++ src/pages/QuieroColaborar/index.js
@@ -70,7 +70,7 @@ class QuieroColaborar extends Component {
                 <Form.Field control={TextArea} label='Mensaje' placeholder='Tu mensaje aquí...' value={message} onChange={(data) => { onSetFormVal('message', data.target.value) }} required />
                 <p><Button type="reset" onClick={this.props.onCancel}>Cancelar</Button> <Button type="submit">Enviar!</Button></p>
                 <Recaptcha
-                  sitekey={ENVAR_API_SITE_KEY}
+                  sitekey={ENVAR_SITE_KEY}
                   render="explicit"
                   verifyCallback={this.verifyCallback}
                 />
